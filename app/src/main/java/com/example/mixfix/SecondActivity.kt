9cc4b7976feaaf7707514b3def014d59652675f7
sarah <pousarah880@gmail.com>
2025-01-09 00:43:36 +0330

    project start

---
+++ app/src/main/java/com/example/mixfix/SecondActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mixfix
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material3.Text
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)  // Replace with actual layout file for the second screen
+    }
+}
